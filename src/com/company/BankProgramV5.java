77d3804e7e68e199d3ea0ba7a7b27122d6920296
mdiallo98 <mamadou1237@gmail.com>
2023-03-29 13:05:41 -0400

    new interfaces for bank program

--- src/com/company/BankProgramV5.java
+++ src/com/company/BankProgramV5.java
@@ -3,4 +3,18 @@ package com.company;
 public interface BankProgramV5 {
 //     this time i will use the public absract keyword but eventually they will be omitted in further versions of the program
 //     abstract keyword means that the logic will not be included in this interface, instead it will be provided by any class that tries to implement it
+        public abstract int getAccNumber();
+        public abstract  int getBalance();
+        public  abstract  boolean isForeign();
+        public  abstract  void  setForeign(boolean isForeign);
+        public abstract  void deposit(int amt);
+        public void boolean hasEnoughColleteral(int amt);
+
+
+
+
+
+
+
+
 }
