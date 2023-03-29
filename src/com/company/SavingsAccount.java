9bf22411728c239c0e8077fdee6ba17b3292eef5
mdiallo98 <mamadou1237@gmail.com>
2023-03-29 13:18:15 -0400

    savings Account that will implement bank interface

---
+++ src/com/company/SavingsAccount.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class SavingsAccount implements BankProgramV5 {
+
+
+    public int getAccNumber() {
+        return 0;
+    }
+
+
+    public int getBalance() {
+        return 0;
+    }
+
+
+    public boolean isForeign() {
+        return false;
+    }
+
+
+    public void setForeign(boolean isForeign) {
+
+    }
+
+
+    public void deposit(int amt) {
+
+    }
+
+
+    public boolean hasEnoughColleteral(int amt) {
+        return false;
+    }
+}
