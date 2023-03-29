fd0da33037c1fc0364d771093f9c333ec4fa2724
mdiallo98 <mamadou1237@gmail.com>
2023-03-29 13:02:11 -0400

    new interfaces for bank program

--- src/com/company/BankProgramV5.java
+++ src/com/company/BankProgramV5.java
@@ -1,5 +1,6 @@
 package com.company;
 
 public interface BankProgramV5 {
+//     this time i will use the public absract keyword but eventually they will be omitted in further versions of the program
+//     abstract keyword means that the logic will not be included in this interface, instead it will be provided by any class that tries to implement it
 }
-z
